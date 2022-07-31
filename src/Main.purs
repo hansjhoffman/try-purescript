fc307aaff10fcd1c813329e0ab4b636d6b9dbb72
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-07-31 11:17:27 -0600

    basic halogen app

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,44 @@
 module Main where
 
 import Prelude
-
 import Effect (Effect)
-import Effect.Console (log)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.VDom.Driver (runUI)
+
+data Action
+  = Increment
+  | Decrement
+
+type State
+  = Int
+
+initialState :: forall i. i -> State
+initialState _ = 0
+
+component :: forall output43 m44 t58 t61. H.Component t58 t61 output43 m44
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    }
+  where
+  render state =
+    HH.div_
+      [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
+      , HH.div_ [ HH.text $ show state ]
+      , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "+" ]
+      ]
+
+  handleAction = case _ of
+    Increment -> H.modify_ \state -> state + 1
+    Decrement -> H.modify_ \state -> state - 1
 
 main :: Effect Unit
-main = do
-  log "Hello World!"
+main =
+  HA.runHalogenAff do
+    body <- HA.awaitBody
+    runUI component unit body
